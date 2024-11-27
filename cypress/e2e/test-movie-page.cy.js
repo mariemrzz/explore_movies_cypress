e1bc0f723296cb09222c93d54ac6950587ffeb99
Maria Morozova <m.morozova@cian.ru>
2024-11-27 20:13:09 +0100

    added comments to a test

--- cypress/e2e/test-movie-page.cy.js
+++ cypress/e2e/test-movie-page.cy.js
@@ -10,17 +10,23 @@ describe('Test movie page', () => {
     }).as('searchBySpecificGenre')
 
     cy.getDataTestId('movieTitleId').find('h1').then((title) => {
+      // Expect a film title and release date to be displayed 
       cy.wait('@searchBySpecificGenre').its('response.body').then((body) => {
         let film = body.results.find((item) => {
           return (item.title + ` (${item.release_date.slice(0, 4)})`) == title.text()
         })
+        // Expect a genre to match the selected genre
         cy.getDataTestId('genresId').find(':selected').invoke('val')
           .should((value) => expect(film.genre_ids).include(Number(value)))
       })
     })
-    cy.getId('moviePoster').find('img').should('have.attr', 'src').should('include','https://image.tmdb.org/')
+    // Expect a film poster to be displayed
+    cy.getId('moviePoster').find('img').should('have.attr', 'src').should('include', 'https://image.tmdb.org/')
+    // Expect imdb rating to be displayed
     cy.contains('span', /^(10(?:\.0+)?|[0-9](?:\.\d+)?)$/i).should('exist')
+    // Expect a film description to be displayed 
     cy.getDataTestId('movieOverviewId').find('p').should('contains.text', 'Description')
+    // Expect like / dislike buttons to be displayed
     cy.getId('likeBtn').should('have.text', '👍')
     cy.getId('dislikeBtn').should('have.text', '👎')
   })
